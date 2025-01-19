7ff6ad968811c30efd457243e5dae3b4d1ec6412
Jperi24 <96599208+Jperi24@users.noreply.github.com>
2025-01-19 15:08:45 -0500

    Update _app.js

--- app/_app.js
+++ app/_app.js
@@ -2,6 +2,9 @@
 import "../app/globals.css"; // Import the global CSS where Tailwind directives are defined
 
 function MyApp({ Component, pageProps }) {
+   <Head>
+        <title>MemeVerse TCG</title> {/* Set the global title */}
+      </Head>
   return <Component {...pageProps} />; // Renders the current page
 }
 
